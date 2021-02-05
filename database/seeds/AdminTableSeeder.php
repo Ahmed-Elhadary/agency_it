e76c964ecdbd982326d3dadf03a5564ad7538dc3
Ahmed-Elhadary <73120189+Ahmed-Elhadary@users.noreply.github.com>
2021-02-05 15:33:52 +0200

    Update AdminTableSeeder.php

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -18,8 +18,8 @@ class AdminTableSeeder extends Seeder
             'name'  =>'مجموعه التحكم الكامل'
         ]);
         $admin = \App\Models\Admin::create([
-            'name' => 'ahmed mohammed',
-            'email' => 'hadary@company.com',
+            'name' => 'Ahmed Hamam',
+            'email' => 'ahmedhamam@agency.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
